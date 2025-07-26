ffd2c2e2fed17c97951f55acc6855e28a9a9de8e
java_learing <aswinsandeep4@gmail.com>
2025-07-26 20:33:25 +0530

    testing ci

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -11,13 +11,14 @@ ALLOWED_HOSTS = ["*"]
 #     }
 # }
 
-DATABASES = {   
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
+        "NAME": os.environ.get("DB_NAME", "default"),
+        "USER": os.environ.get("DB_USER", "default"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "default"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
+
