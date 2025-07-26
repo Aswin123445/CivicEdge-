b5c76e87cda44a65efcbed10c79a1143d327b687
java_learing <aswinsandeep4@gmail.com>
2025-07-26 19:58:13 +0530

    testing ci

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -17,7 +17,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST", "postgres"),
+        "HOST": "postgres" if os.getenv("GITHUB_ACTIONS") == "true" else os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
