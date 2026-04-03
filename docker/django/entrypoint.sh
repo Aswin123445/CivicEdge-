aa30ce3cf29845b96c6a8de6e79d3ba831ae3ce3
java_learing <aswinsandeep4@gmail.com>
2026-04-03 21:08:16 +0530

    added .sh file in docker updated djanog docker file for automatic migration

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "⏳ Waiting for database..."
+
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 0.5
+done
+
+echo "✅ Database is up"
+
+echo "🚀 Running migrations..."
+python manage.py migrate --noinput
+
+echo "📦 Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "🎯 Starting server..."
+exec "$@"
