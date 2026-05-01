2cff123544f51decf28567b0e14386cb3e01be32
java_learing <aswinsandeep4@gmail.com>
2026-05-01 13:36:48 +0530

    implemented lazy module loading poll_similarity

--- backend/apps/ai/poll_similarity.py
+++ backend/apps/ai/poll_similarity.py
@@ -1,9 +1,18 @@
 from sentence_transformers import SentenceTransformer, util
 
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+_model = None
+
+
+def get_model():
+    global _model
+    if _model is None:
+        _model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    return _model
 
 
 def question_similarity(old, new):
+    model = get_model()
+
     emb1 = model.encode(old, convert_to_tensor=True)
     emb2 = model.encode(new, convert_to_tensor=True)
 
